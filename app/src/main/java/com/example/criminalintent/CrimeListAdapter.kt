f9edf61694c2e46529e111db94ed12f5ce879b1c
JhonFarell <unluckyrobbers@gmail.com>
2022-11-11 15:16:06 +0300

    simplified multiview

--- app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -7,7 +7,6 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.criminalintent.databinding.ListItemCrimeBinding
-import com.example.criminalintent.databinding.ListItemCrimeHardBinding
 
 class CrimeHolder (private val binding: ListItemCrimeBinding)
         : RecyclerView.ViewHolder(binding.root) {
@@ -19,65 +18,35 @@ class CrimeHolder (private val binding: ListItemCrimeBinding)
                                 View.VISIBLE
                         } else {View.INVISIBLE}
 
+                        binding.isCriminal.visibility = if (crime.isCriminal){
+                                View.VISIBLE
+                        }else {View.INVISIBLE}
+
                         binding.root.setOnClickListener {
                                 Toast.makeText(binding.root.context,
                                         "Fuck you Joe Rogan!",
                                         Toast.LENGTH_SHORT).show()
                         }
+                        binding.isCriminal.setOnClickListener {
+                                Toast.makeText(binding.root.context,
+                                                "I'm calling the police!",
+                                                Toast.LENGTH_SHORT).show()
+                        }
                 }
 
         }
-
-class CrimeHolderExtreme (private val binding: ListItemCrimeHardBinding)
-        :RecyclerView.ViewHolder(binding.root) {
-                fun bind (crime: CrimeModel) {
-                        binding.crimeNumber.text = crime.title
-                        binding.crimeDate.text = crime.date.toString()
-                        binding.callThePolice.setOnClickListener {
-                                Toast.makeText(binding.root.context,
-                                        "Daaaaaaaaamn it's a crime... :(",
-                                        Toast.LENGTH_SHORT).show()
-                        }
-
-
-
-                }
-
-}
 
 class CrimeListAdapter(private val crimes: List<CrimeModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
                 val inflater = LayoutInflater.from(parent.context)
-                val bindingCrime = ListItemCrimeBinding.inflate(inflater,parent,false)
-                val bindingHardCrime = ListItemCrimeHardBinding.inflate(inflater, parent, false)
-                return if (viewType == HARD_CRIME) {
-                        CrimeHolderExtreme(bindingHardCrime)
-                }
-                        else {CrimeHolder(bindingCrime)}
-
-                }
-
+                val bindingCrime = ListItemCrimeBinding.inflate(inflater, parent, false)
+                return CrimeHolder(bindingCrime)
+        }
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-               var crime = crimes[position]
+                var crime = crimes[position]
 
-                if (getItemViewType(position)== HARD_CRIME) {
-                        (holder as CrimeHolderExtreme).bind(crime)
-                } else {(holder as CrimeHolder).bind(crime)}
+                (holder as CrimeHolder).bind(crime)
         }
 
         override fun getItemCount(): Int = crimes.size
-
-        override fun getItemViewType(position: Int): Int {
-                var crime = crimes[position]
-                return when {
-                        crime.isCriminal -> HARD_CRIME
-                        else -> CRIME
-                }
-        }
-
-        companion object type {
-                val HARD_CRIME = 0
-                val CRIME = 1
-        }
-
 }
